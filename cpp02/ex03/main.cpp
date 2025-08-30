d67fa09cc62cf60857dafcccdbe016635f46f793
zakaria <zakariakhourbach@gmail.com>
2025-08-30 13:10:07 +0100

    add main

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -1,10 +1,17 @@
 #include "Point.hpp"
 
-int main() {
-   
-    Point a(0,0);
-    Point c(0,20);
-    Point b(10,30);
+int main()
+{
+
+    Point a(0, 0);
+    Point c(0, 20);
+    Point b(10, 30);
+    Point p(8, 28);
+
+    if (bsp(a, b, c, p))
+        std::cout << "its inside "<<std::endl;
+    else
+        std::cout << "its outside "<<std::endl;
 
     return 0;
 }
