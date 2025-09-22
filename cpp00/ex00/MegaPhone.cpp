dd50932fd94f1e5050493afa0b48efd2cda9bc6c
Zakaria Khourbach <zkhourba@e3r7p14.1337.ma>
2025-09-22 11:54:37 +0100

    use endl in cpp00

--- cpp00/ex00/MegaPhone.cpp
+++ cpp00/ex00/MegaPhone.cpp
@@ -6,7 +6,7 @@ int main(int argc,char **argv)
     std::string str;
     if(argc == 1)
     {
-        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
         return (0);
     }
     for ( int i = 1; i < argc; i++)
@@ -15,10 +15,9 @@ int main(int argc,char **argv)
         for (size_t j = 0; j < str.size(); j++)
         {
             std::cout<<(char)std::toupper(str[j]);
-            
         }
         
     }
-    std::cout <<'\n';
+    std::cout <<std::endl;
     return(0);
 }
