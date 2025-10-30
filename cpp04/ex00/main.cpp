3d1a9ed16a241256a416a10e00fe5219b4bf3581
Zakaria Khourbach <zkhourba@e1r11p1.1337.ma>
2025-10-30 15:08:56 +0100

    fix unkown to unknown

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -3,12 +3,13 @@
 #include "WrongCat.hpp"
 int main()
 {
+    
     const Animal *meta = new Animal();
     const Animal *j = new Dog();
     const Animal *i = new Cat();
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); // will output the cat sound!
+    i->makeSound(); 
     j->makeSound();
     meta->makeSound();
     delete i;
