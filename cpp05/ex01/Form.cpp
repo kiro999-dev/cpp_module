7a833900e8b33ce3a615035f6acaa42f848cd76e
Zakaria Khourbach <zkhourba@e2r6p1.1337.ma>
2025-12-09 17:36:37 +0100

    add form class

---
+++ cpp05/ex01/Form.cpp
@@ -0,0 +1,61 @@
+#include "Form.hpp"
+Form::Form():_Name("Boss Form"),_IsSigned(0),
+_GradeSign(1),
+_GradeExecute(1){}
+Form::Form(std::string name,short gradeSign,short gradeExecute):_Name(name),_IsSigned(0),
+_GradeSign(gradeSign),
+_GradeExecute(gradeExecute)
+{
+    if(gradeExecute > 150 || gradeSign > 150)
+        throw GradeTooLowException();
+    if(gradeExecute < 1 || gradeSign < 1)
+        throw GradeTooHighException();
+}
+Form::Form(const Form &other):_Name(other._Name),_IsSigned(other._IsSigned),
+_GradeSign(other._GradeSign),
+_GradeExecute(other._GradeExecute)
+{
+}
+const Form Form::operator=(const Form&other)
+{
+    _IsSigned = other._IsSigned;
+    return *this;
+}
+const char *Form::GradeTooHighException::what() const throw()
+{
+    return "Grade Too High";
+}
+const char *Form::GradeTooLowException::what() const throw()
+{
+    return "Grade Too Low";
+}
+const std::string &Form::GetName() const
+{
+    return _Name;
+}
+bool  Form::GetIsSigned() const
+{
+    return _IsSigned;
+}
+const short& Form::GetGradeSign() const
+{
+    return _GradeSign;
+}
+const short& Form::GetGradeExecute() const
+{
+    return _GradeExecute;
+}
+void  Form::SetIsSigned(bool fact)
+{
+    _IsSigned = fact;
+}
+std::ostream& operator<<(std::ostream& os, const Form& obj)
+{
+    os<<"NAME: "<<obj.GetName()<<std::endl;
+    os<<"IS SIGNED: "<<obj.GetIsSigned() ? "YES" : "NO"; 
+    os <<std::endl;
+    os<<"GRADE REQUIRED TO SIGN IT: "<<obj.GetGradeSign()<<std::endl;
+    os<<"GRADE REQUIRED TO EXECUTE IT: "<<obj.GetGradeSign()<<std::endl;
+    return os;
+}
+Form::~Form(){}
---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,38 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+#include "Bureaucrat.hpp"
+class Form
+{
+private:
+    const std::string _Name;
+    bool _IsSigned;
+    const short _GradeSign;
+    const short _GradeExecute;
+public:
+    Form(std::string,short,short);
+    Form();
+    Form(const Form&other);
+    const Form operator=(const Form&other);
+    class GradeTooHighException : public std::exception
+    {
+        public:
+            GradeTooHighException();
+           virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception
+    {
+        public:
+            GradeTooLowException();
+           virtual const char *what()  const throw();
+    };
+    ~Form();
+    const std::string &GetName() const;
+    bool  GetIsSigned() const;
+    const short& GetGradeSign() const;
+    const short& GetGradeExecute() const;
+    void  SetIsSigned(bool );
+};
+std::ostream& operator<<(std::ostream& os, const Form& obj);
+
+#endif
