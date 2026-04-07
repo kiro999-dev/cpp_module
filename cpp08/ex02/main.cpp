662fbd1a654f58f3f6864b20d0a0c212855f7db1
Zakaria Khourbach <zkhourba@e1r12p5.1337.ma>
2026-04-07 12:20:57 +0100

    update test cases

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,18 +1,26 @@
 
 #include "MutantStack.hpp"
+#include <list>
 int main()
 {
     MutantStack<int> mstack;
+    std::list<int> numberList;
+    numberList.push_front(5);
+    numberList.push_front(17);
     mstack.push(5);
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
+    std::cout <<"MuntanStack: "<< mstack.top() << std::endl;
+    std::cout <<"List: "<< numberList.front() << std::endl;
+    numberList.pop_front();
     mstack.pop();
-    std::cout << mstack.size() << std::endl;
+    std::cout << "MuntanStack: "<< mstack.size() << std::endl;
+    std::cout <<"List: "<< numberList.size() << std::endl;
+
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
+    std::cout<<"MutantStack : \n";
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
@@ -21,6 +29,21 @@ int main()
     {
         std::cout << *it << std::endl;
         ++it;
+    }
+    
+    numberList.push_back(3);
+    numberList.push_back(5);
+    numberList.push_back(737);
+    numberList.push_back(0);
+    std::cout<<"List : \n";
+    std::list<int>::iterator it1 = numberList.begin();
+    std::list<int>::iterator ite2 = numberList.end();
+    ++it1;
+    --it1;
+    while (it1 != ite2)
+    {
+        std::cout << *it1 << std::endl;
+        ++it1;
     }
     std::stack<int> s(mstack);
     return 0;
